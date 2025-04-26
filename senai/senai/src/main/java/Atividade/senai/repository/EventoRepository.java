ba34e2ead1732dd10e444731164cf02b21a45852
Bruno Gomes <brunogomesestudos@gmail.com>
2025-04-26 00:15:15 -0300

    Delete senai/senai directory

--- senai/senai/src/main/java/Atividade/senai/repository/EventoRepository.java
+++
@@ -1,9 +0,0 @@
-package Atividade.senai.repository;
-
-import Atividade.senai.model.Evento;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EventoRepository extends JpaRepository<Evento, Long> {
-}
