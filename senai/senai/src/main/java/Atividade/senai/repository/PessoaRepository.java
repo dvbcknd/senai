ab02d64a679dfbc47c36100d28657e6c554b457b
Bruno Gomes <brunogomesestudos@gmail.com>
2025-05-09 18:57:27 -0300

    Atualização do nome de usuario e senha da autenticacao

--- senai/senai/src/main/java/Atividade/senai/repository/PessoaRepository.java
+++
@@ -1,9 +0,0 @@
-package Atividade.senai.repository;
-
-import Atividade.senai.model.Pessoa;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
-}
