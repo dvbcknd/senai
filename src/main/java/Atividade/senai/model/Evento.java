f014a229715f41344479da675d2ef4b319d19a66
dvbcknd <189028215+dvbcknd@users.noreply.github.com>
2025-05-09 18:57:27 -0300

    Atualização do nome de usuario e senha da autenticacao

---
+++ src/main/java/Atividade/senai/model/Evento.java
@@ -0,0 +1,30 @@
+package Atividade.senai.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class Evento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+    private String descricao;
+
+    private LocalDate data;
+
+    // Getters e Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getTitulo() { return titulo; }
+    public void setTitulo(String titulo) { this.titulo = titulo; }
+
+    public String getDescricao() { return descricao; }
+    public void setDescricao(String descricao) { this.descricao = descricao; }
+
+    public LocalDate getData() { return data; }
+    public void setData(LocalDate data) { this.data = data; }
+}
